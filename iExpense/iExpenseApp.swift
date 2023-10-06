32832c6405ea433b9d17a4c39d8537f64e2ae0b3
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-06 09:27:50 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dennis Mutwiri Mugambi on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
