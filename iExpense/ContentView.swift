0b2c766128d9b531e16d18569566b794af3dd43c
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-06 10:38:31 +0300

    implements @AppStorage to simplify reading and writing to preferences(UserDefaults)

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         
         Button("Tap count: \(tapCount)") {
-            tapCount += 1
-            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
-        }
+                    tapCount += 1
+                }
         
     }
 
