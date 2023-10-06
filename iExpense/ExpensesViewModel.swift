69f700591612eba093f775e9cdb10d5af3b3f7ba
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-06 12:03:12 +0300

    adds expense and saves to user defaults

--- iExpense/ExpensesViewModel.swift
+++ iExpense/ExpensesViewModel.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodeItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodeItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
