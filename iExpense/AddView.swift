e7892199e127f07c87c92a22cd9e57645a627c1a
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2023-10-06 12:12:25 +0300

    polishes ui

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -15,6 +15,8 @@ struct AddView: View {
     
     @ObservedObject var expenses: Expenses
     
+    @Environment(\.dismiss) var dismiss
+    
     let types = ["Business", "Personal"]
     
     var body: some View {
@@ -36,6 +38,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
